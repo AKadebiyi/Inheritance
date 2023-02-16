59710708e9ac00a5791066601493fda570eef46c
Adeola Adebiyi <adeola_adebiyi1@baylor.edu>
2023-02-16 13:48:07 -0600

    created gitignore and venv; then class practice

--- f_polymorphism_demo.py
+++ f_polymorphism_demo.py
@@ -2,6 +2,7 @@
 
 import f_animals as animals
 
+
 def main():
     # Create a Mammal object, a Dog object, and
     # a Cat object.
@@ -9,23 +10,35 @@ def main():
     dog = animals.Dog()
     cat = animals.Cat()
 
-
     # Display information about each one.
     print('Here are some animals and')
     print('the sounds they make.')
     print('--------------------------')
-    mammal.show_species()
-    mammal.make_sound()
+    # mammal.show_species()
+    # mammal.make_sound()
 
-    print()
+    # print()
 
-    dog.show_species()
-    dog.make_sound()
+    # dog.show_species()
+    # dog.make_sound()
 
-    print()
+    # print()
 
-    cat.show_species()
-    cat.make_sound()
+    # cat.show_species()
+    # cat.make_sound()
+
+    display_info(mammal)
+    display_info(dog)
+    display_info(cat)
+
+
+def display_info(creature):
+    if isinstance(creature, animals.Mammal):
+        creature.show_species()
+        creature.make_sound()
+    else:
+        print('That is not a mammal!')
+
 
 # Call the main function.
 main()
